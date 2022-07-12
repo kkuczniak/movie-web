abbc81b1cb4e62d798fceebb31800addde095465
kkuczniak <51820616+kkuczniak@users.noreply.github.com>
2022-07-12 22:11:38 +0200

    fixed row arrow

--- components/Row.tsx
+++ components/Row.tsx
@@ -31,7 +31,7 @@ function Row({ title, movies }: Props) {
       </h2>
       <div className='group relative'>
         <ChevronLeftIcon
-          className='absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100'
+          className='absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 fill-CustomHoneyYellow'
           onClick={() => handleClick('left')}
         />
         <div
@@ -44,7 +44,7 @@ function Row({ title, movies }: Props) {
         </div>
 
         <ChevronRightIcon
-          className='absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100'
+          className='absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 fill-CustomHoneyYellow '
           onClick={() => handleClick('right')}
         />
       </div>
