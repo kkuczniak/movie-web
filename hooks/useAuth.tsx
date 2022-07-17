89c818a01d2afc967f1821f13ac0c0fcb9b4e902
kkuczniak <51820616+kkuczniak@users.noreply.github.com>
2022-07-17 23:46:28 +0200

    deploy fix

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,7 +1,6 @@
-
-import { useRouter } from 'next/router'
-import { createContext, useContext, useEffect, useMemo, useState } from 'react'
-import { auth } from '../firebase'
+import { useRouter } from 'next/router';
+import { createContext, useContext, useEffect, useMemo, useState } from 'react';
+import { auth } from '../firebase';
 
 import {
   createUserWithEmailAndPassword,
@@ -9,16 +8,15 @@ import {
   signInWithEmailAndPassword,
   signOut,
   User,
-} from 'firebase/auth'
-
+} from 'firebase/auth';
 
 interface AuthInterface {
-  user: User | null
-  signUp: (email: string, password: string) => Promise<void>
-  signIn: (email: string, password: string) => Promise<void>
-  logout: () => Promise<void>
-  error: string | null
-  loading: boolean
+  user: User | null;
+  signUp: (email: string, password: string) => Promise<void>;
+  signIn: (email: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+  error: string | null;
+  loading: boolean;
 }
 
 const AuthContext = createContext<AuthInterface>({
@@ -28,58 +26,63 @@ const AuthContext = createContext<AuthInterface>({
   logout: async () => {},
   error: null,
   loading: false,
-})
+});
 
 interface AuthProviderProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
-export const AuthProvider = ({children}:AuthProviderProps) => {
-  const [loading, setLoading] = useState(false)
-  const [user, setUser] = useState<User | null>(null)
-  const router = useRouter()
- 
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
+  const router = useRouter();
+
   const signUp = async (email: string, password: string) => {
-    setLoading(true)
+    setLoading(true);
 
-    await createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
-      setUser(userCredential.user)
-      router.push('/')
-      setLoading(false)
-    }
-    ).catch((error) => alert(error.message)).finally(()=> setLoading(false))
-  }
-
-  const signIn = async (email: string, password: string) => {
-    setLoading(true)
-    await signInWithEmailAndPassword(auth, email, password)
+    await createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        setUser(userCredential.user)
-        router.push('/')
-        setLoading(false)
+        setUser(userCredential.user);
+        router.push('/');
+        setLoading(false);
       })
       .catch((error) => alert(error.message))
-      .finally(() => setLoading(false))
-  }
+      .finally(() => setLoading(false));
+  };
+
+  const signIn = async (email: string, password: string) => {
+    setLoading(true);
+    await signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setUser(userCredential.user);
+        router.push('/');
+        setLoading(false);
+      })
+      .catch((error) => alert(error.message))
+      .finally(() => setLoading(false));
+  };
 
   const logout = async () => {
-    setLoading(true)
+    setLoading(true);
 
     signOut(auth)
       .then(() => {
-        setUser(null)
+        setUser(null);
       })
       .catch((error) => alert(error.message))
-      .finally(() => setLoading(false))
-  }
+      .finally(() => setLoading(false));
+  };
 
-  const memoedValue = useMemo(() => ({user, signUp, signIn, loading, logout}),[user, loading])
+  const memoedValue = useMemo(
+    () => ({ user, signUp, signIn, loading, logout }),
+    [user, loading, error]
+  );
 
-  return <AuthContext.Provider value={}>
-    {children}
-  </AuthContext.Provider>
-}
+  return (
+    <AuthContext.Provider value={memoedValue}>{children}</AuthContext.Provider>
+  );
+};
 
-export default function useAuth(){
-  return useContext(AuthContext)
+export default function useAuth() {
+  return useContext(AuthContext);
 }
